f06b8a1a65c0400eb4721632c666d888642317e0
Kyungmin97 <87704434+Kyungmin97@users.noreply.github.com>
2022-02-09 18:18:15 +0000

    123

--- DFS_BFS.py
+++ DFS_BFS.py
@@ -29,6 +29,59 @@ graph=[[0,1,2],[3,4,INF],[7,INF,9]]
 graph=[[] for _ in range(3)]
 print(graph)
 """
+"""
+n,m=map(int,input().split())
+graph=[]
+for i in range(n):
+  graph.append(list(map(int,input())))
+
+def dfs(x,y):
+  if x<0 or y<0 or x>=n or y>=m:
+    return False
+  
+  if graph[x][y] == 0:
+    graph[x][y] = 1
+    dfs(x-1,y)
+    dfs(x+1,y)
+    dfs(x,y-1)
+    dfs(x,y+1)
+    return True
+  return False
+
+result=0
+for i in range(n):
+  for j in range(m):
+    if dfs(i,j)==True:
+      result+=1
+
+print(result)
+"""
+from collections import deque
+
+n,m=map(int,input().split())
+graph=[]
+for _ in range(n):
+  graph.append(list(map(int,input())))
+
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+
+def bfs(x,y):
+  q=deque()
+  q.append((x,y))
+  while q:
+    x,y=q.popleft()
+    for i in range(4):
+      nx=x+dx[i]
+      ny=y+dy[i]
+
+      if nx<0 or ny<0 or nx>=n or ny>=m or graph[nx][ny]==0:
+        continue
+      if graph[nx][ny] == 1:
+        graph[nx][ny]=graph[x][y]+1
+        q.append((nx,ny))
+  return graph[n-1][m-1]
+print(bfs(0,0))
 
 
 
