01f384db4643be18f09f9dc43b8026f075a3382f
Kyungmin97 <87704434+Kyungmin97@users.noreply.github.com>
2022-02-09 17:57:32 +0000

    dfs

--- main.py
+++ main.py
@@ -44,3 +44,114 @@ while result>1:
 
 print(cnt)
 """
+
+#구현 ex1
+"""
+n=int(input())
+plans=input().split()
+x,y=1,1
+dx=[0,0,-1,1]
+dy=[-1,1,0,0]
+move=['w','s','a','d']
+
+for plan in plans:
+  for i in range(4):
+    if plan==move[i]:
+      nx=x+dx[i]
+      ny=y+dy[i]
+  if nx<1 or ny<1 or nx>n or ny>n:
+    continue
+  x,y=nx,ny
+print(x,y)
+"""
+
+#구현 ex2
+"""
+n=int(input())
+count=0
+for i in range(n+1):
+  for j in range(60):
+    for k in range(60):
+      if '3' in str(i)+str(j)+str(k):
+        count +=1
+print(count)
+"""
+
+#구현 1
+"""
+n=input()
+x,y=int(n[1]),ord(n[0])-ord('a')+1
+
+dx=[2,2,-2,-2,1,1,-1,-1]
+dy=[1,-1,1,-1,2,-2,2,-2]
+cnt=0
+for i in range(8):
+  nx=x+dx[i]
+  ny=y+dy[i]
+
+  if nx<1 or ny<1 or nx>8 or ny>8:
+    continue
+  cnt+=1
+print(cnt)
+"""
+
+#구현 2
+"""
+n,m=map(int,input().split())
+x,y,d=map(int,input().split())
+nlist=[]
+for _ in range(n):
+  nlist.append(list(map(int,input().split())))
+
+dx=[0,-1,0,1]
+dy=[-1,0,1,0]
+
+nlist[y][x]=2
+while True:
+  for i in range(4):
+    d=d-1
+    if d==-1:
+      d=3
+    nx=x+dx[d]
+    ny=y+dy[d]
+    if nx<0 or ny<0 or nx>m or ny>n or nlist[ny][nx]!=0:
+      continue
+    nlist[y][x]=2
+    x,y=nx,ny
+
+  nx=x-dx[d]
+  ny=y-dy[d]
+  if nx<0 or ny<0 or nx>m or ny>n or nlist[ny][nx]==1:
+    break
+    
+cnt=0
+for i in range(n):
+  cnt+=nlist[i].count(2)
+print(cnt)
+print(nlist)
+"""
+"""
+n,m=map(int,input().split())
+d=[[0]* m for _ in range(n)]
+x,y,direction=map(int,input().split())
+
+d[x][y]=1
+
+nlist=[]
+for _ in range(n):
+  nlist.append(list(map(int,input().split())))
+
+dx=[-1,0,1,0]
+dy=[0,1,0,-1]
+"""
+
+import DFS_BFS
+
+
+
+
+
+
+
+
+
